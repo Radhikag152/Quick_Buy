6a2c6a590cf1a9fd80cc3c5316738a8296bbb972
ajaiaugustine14 <ajaiaugustine94@gmail.com>
2021-01-31 00:15:55 +0530

    Flipkart Buy Product

--- scenarios/flipkart.feature
+++ scenarios/flipkart.feature
@@ -3,6 +3,6 @@ Feature: Google Search
   @Smoke
   Scenario: Sample Test
     Given get 'https://www.flipkart.com/'
-    When I login to Flipkart with username 'username' and password 'password'
-#    When search for 'qaf github infostretch'
+    When I login to Flipkart with username 'ajaiaugustine94@gmail.com' and password 'flippass@123'
+    When Order the product 'Samsung Galaxy M31 (Space Black, 64 GB)' from Flipkart
 #    Then verify link with partial text 'qaf' is present
--- src/test/java/com/quick/buy/utils/FlipkartBuyUtils.java
+++ src/test/java/com/quick/buy/utils/FlipkartBuyUtils.java
@@ -1,8 +1,15 @@
 package com.quick.buy.utils;
+import com.qmetry.qaf.automation.ui.WebDriverTestBase;
+import com.qmetry.qaf.automation.ui.webdriver.QAFExtendedWebElement;
+import java.util.ArrayList;
+import java.util.Set;
+
+import com.qmetry.qaf.automation.core.TestBaseProvider;
 
 import org.openqa.selenium.Keys;
 
 import com.qmetry.qaf.automation.step.CommonStep;
+import com.qmetry.qaf.automation.ui.WebDriverTestBase;
 
 public class FlipkartBuyUtils {
 	
@@ -13,6 +20,35 @@ public class FlipkartBuyUtils {
 		CommonStep.sendKeys(passowrd, "flipkart.sigin.password");
 		CommonStep.click("flipkart.sigin.button");
 		CommonStep.waitForEnabled("flipkart.Account.label", 11);
+	}
+	
+	public static void OrderProduct(String order) throws InterruptedException
+	{
+		CommonStep.waitForEnabled("flipkart.search.textbox", 11);
+		CommonStep.sendKeys(order, "flipkart.search.textbox");
+		CommonStep.click("flipkart.search.button");
+		String productXpath = "//div[contains(text(),'"+order+"')]";
+		String productHeadingXpath = "//span[contains(text(),'"+order+"')]";
+		CommonStep.waitForEnabled(productXpath, 11);
+		CommonStep.click(productXpath);
+		Thread.sleep(1000);
+		CommonStep.switchToWindow("1");
+		Thread.sleep(1000);
+		boolean isDisplayed = false;
+		do {
+			CommonStep.waitForEnabled(productHeadingXpath, 11);
+			if(CommonStep.verifyVisible("flipkart.BuyNow.button"))
+			{
+				CommonStep.click("flipkart.BuyNow.button");
+				isDisplayed = true;
+			}
+			else
+			{
+				new WebDriverTestBase().getDriver().navigate().refresh();
+			}
+			
+		}while(isDisplayed==false);
+		CommonStep.waitForEnabled("flipkart.DeliverHere.button", 11);
 	}
 
 }
